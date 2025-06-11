a4b64613a9dd93cd94f22b623eae4a8a82a956d8
Federico López <162905321+pancoded@users.noreply.github.com>
2025-06-10 23:41:37 -0300

    Update Post.java

--- src/main/java/com/example/blog/model/Post.java
+++ src/main/java/com/example/blog/model/Post.java
@@ -7,8 +7,6 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
 import java.time.Instant;
 import java.util.List;
 import lombok.AllArgsConstructor;
